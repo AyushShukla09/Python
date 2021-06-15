16d16deb0520b50231e13f5ed8b12c960dbd4dfb
AyushShukla09 <hbkayushshukla09@gmail.com>
2021-06-15 13:42:57 +0530

    added comments to array manipulations

--- arrays/array_manipulation.py
+++ arrays/array_manipulation.py
@@ -1,3 +1,24 @@
+'''
+Starting with a 1-indexed array of zeros and a list of operations, for each operation add a value to each the array element between two given indices, inclusive. Once all operations have been performed, return the maximum value in the array.
+
+Example
+
+n=  1
+queries =
+    a b k
+    1 5 3
+    4 8 7
+    6 9 1
+index->	 1 2 3  4  5 6 7 8 9 10
+	    [0,0,0, 0, 0,0,0,0,0, 0]
+	    [3,3,3, 3, 3,0,0,0,0, 0]
+	    [3,3,3,10,10,7,7,7,0, 0]
+	    [3,3,3,10,10,8,8,8,1, 0]
+
+'''
+
+
+
 def arrayManipulation(n, queries):
     # Write your code here
     arr = [0] * (n + 2)
